cfc1cd2cf1d8495c38ff585abf041ea188270a0f
OUCHUNYU <ouchunyu@yahoo.com>
2016-08-10 16:54:12 -0700

    bootstarp

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,11 @@
 import React                  from 'react'
-import Home                   from '../Home/Home.js'
+import DashBoard              from '../DashBoard/DashBoard.js'
 import { Route, IndexRoute }  from 'react-router'
 
 
 module.exports = (
-  <Route path="/" component={Home}>
-    <IndexRoute component={Home} />
-    <Route path="profile" component={Home} />
+  <Route path="/" component={DashBoard}>
+    <IndexRoute component={DashBoard} />
+    <Route path="/dashboard" component={DashBoard} />
   </Route>
 )
