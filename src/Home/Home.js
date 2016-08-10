71c8749cb27a6ce86bdd10a4ebb2ab05944bed1c
OUCHUNYU <ouchunyu@yahoo.com>
2016-08-09 23:22:15 -0700

    delete intial files

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import './Home.css';
 
 class Home extends Component {
@@ -7,12 +6,8 @@ class Home extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Fancy TODO</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
