33958821ce66d4682bdaf59ac887d04a52b354ce
OUCHUNYU <ouchunyu@yahoo.com>
2016-08-09 23:18:21 -0700

    make home dir

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,11 @@
-import React                 from 'react'
-import App                   from '../App.js'
-import { Route, IndexRoute } from 'react-router'
+import React                  from 'react'
+import Home                   from '../Home/Home.js'
+import { Route, IndexRoute }  from 'react-router'
 
 
 module.exports = (
-  <Route path="/" component={App}>
-    <IndexRoute component={App} />
-    <Route path="profile" component={App} />
+  <Route path="/" component={Home}>
+    <IndexRoute component={Home} />
+    <Route path="profile" component={Home} />
   </Route>
 )
